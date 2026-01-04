e3f797e06082d8669a643b94e85b9f8355ffb466
WYH <wyh@WYHdeMacBook-Air.local>
2026-01-04 20:13:31 +0800

    fix

---
+++ util/nets/nets_test.go
@@ -0,0 +1,12 @@
+package nets
+
+import (
+	"testing"
+)
+
+func TestIsUsableAddr(t *testing.T) {
+	//fmt.Println(IsUsableAddr(":8080"))
+	//fmt.Println(IsUsableAddr(":8070"))
+	//fmt.Println(IsUsableAddr(":8060"))
+	//fmt.Println(IsUsableAddr(":8050"))
+}
